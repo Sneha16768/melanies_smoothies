d7783bd1315ada50f7d33d52ef2a08ff1e6fddc2
Sneha16768 <eng18me0064.snehas@gmail.com>
2025-03-16 13:12:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ cnx = st.connection("snowflake")
 session = cnx.session() 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 # Convert the Snowpark Dataframe to Pandas Dataframe to use the LOC function
@@ -39,7 +39,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       #  st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
@@ -51,6 +51,8 @@ if ingredients_list:
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     # st.write(my_insert_stmt)
+    #st.stop()
+    
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
